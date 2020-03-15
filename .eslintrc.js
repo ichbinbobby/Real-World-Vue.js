5e9033deb9f7c9ba1107433f279310a6dbbe6649
SaikaVA <nthanbinh@gmail.com>
2020-03-15 12:33:39 +0100

    Completed Real World Vue.js Course

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
+  plugins: ['vue'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-};
+}
---
+++ src/views/EventList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventServices from '@/services/EventServices.js'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventServices.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:' + error.response)
+      })
+  }
+}
+</script>
+
+<style></style>
